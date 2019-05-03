7794f8142514cee899cdafec2e0f44158e84e974
Robert Sheehan <robertnsheehan@gmail.com>
2019-05-03 16:37:17 +0100

    Some notes added.

--- DBR_Design/DBR_Design/Main.cpp
+++ DBR_Design/DBR_Design/Main.cpp
@@ -3,6 +3,8 @@
 #endif
 
 // Implementation of the code needed to compute the parameters of a DBR grating in optical waveguide with sidewall corrugation
+// See files DBR_Analysis.h and DBR_Analysis.cpp from old EIM code
+// Re-format that code so it's compatible with new dispersion calculation code. 
 // R. Sheehan 3 - 5 - 2019
 
 int main() 
